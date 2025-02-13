c230966dd362e645bd1796438a493f967fe8600d
Thilina Wijerathna <thilinadrwijerathna@gmail.com>
2025-02-13 14:38:21 +0530

    update
    update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -9,43 +9,44 @@ const HomePage = () => {
 const [showProductModal, setShowProductModal] = useState(false);
 const [currentProduct, setCurrentProduct] = useState(null);
 const handleAddProduct = () => {
-setCurrentProduct(null);
-setShowProductModal(true);
+    setCurrentProduct(null);
+    setShowProductModal(true);
+    };
+    const handleEditProduct = (product) => {
+        setCurrentProduct(product);
+        setShowProductModal(true);
+    };
+    const handleProductSubmit = (productData) => {
+    setShowProductModal(false);
 };
-const handleEditProduct = (product) => {
-setCurrentProduct(product);
-setShowProductModal(true);
-};
-const handleProductSubmit = (productData) => {
-setShowProductModal(false);
-};
-return (
+    return (
 <>
 <Header />
 <div className="mt-5">
-<Button onClick={handleAddProduct}>Add Product</Button>
-<ProductModal
-show={showProductModal}
-onHide={() => setShowProductModal(false)}
+    <Button onClick={handleAddProduct}>Add Product</Button>
+    <ProductModal
+    show={showProductModal}
+    onHide={() => setShowProductModal(false)}
 
-onSubmit={handleProductSubmit}
-initialProduct={currentProduct}
+    onSubmit={handleProductSubmit}
+    initialProduct={currentProduct}
 />
-<h1 className="text-center mt-4">All Products</h1>
-<div className="row mb-3">
-{productsData.map((product) => (
-<div className="col-md-3 mb-3" key={product.id}>
+    <h1 className="text-center mt-4">All Products</h1>
+    <div className="row mb-3">
+    {productsData.map((product) => (
+    <div className="col-md-3 mb-3" key={product.id}>
 <ProductCard product={product} />
-<Button onClick={() => handleEditProduct(product)}
-className="mt-2">
-Edit Product
-</Button>
-</div>
-))}
+    <Button onClick={() => handleEditProduct(product)}
+    className="mt-2">
+    Edit Product
+    </Button>
+    </div>
+    ))}
 </div>
 </div>
 <Footer />
 </>
 );
 };
+
 export default HomePage;s
